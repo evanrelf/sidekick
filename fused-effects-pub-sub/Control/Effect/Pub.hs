5794e3b8d069d347e7a222aff75e828b2a375595
Evan Relf <evan@evanrelf.com>
2020-11-28 15:42:21 -0800

    Write `Pub` and `Sub` effects

--- fused-effects-pub-sub/Control/Effect/Pub.hs
+++ fused-effects-pub-sub/Control/Effect/Pub.hs
@@ -1,1 +1,25 @@
-module Control.Effect.Pub where
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+
+module Control.Effect.Pub
+  (
+  -- * Pub effect
+    Pub (..)
+  , pub
+  -- * Re-exports
+  , Algebra
+  , Has
+  , run
+  )
+where
+
+import Control.Algebra (Algebra, Has, run, send)
+import Data.Kind (Type)
+
+
+data Pub a (m :: Type -> Type) k where
+  Pub :: a -> Pub a m ()
+
+
+pub :: Has (Pub a) sig m => a -> m ()
+pub x = send (Pub x)
