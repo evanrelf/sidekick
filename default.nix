054856ae43ea7aa7c7a5a3dcff5f7719e0495ceb
Evan Relf <evan@evanrelf.com>
2021-06-01 18:22:28 -0700

    Fix `streamly-fsnotify` problems

--- default.nix
+++ default.nix
@@ -4,6 +4,12 @@ let
       packages = {
         "sidekick" = pkgs.nix-gitignore.gitignoreSource [ ./.nixignore ] ./.;
       };
+      overrideCabal = {
+        "streamly-fsnotify" = old: {
+          broken = false;
+          jailbreak = true;
+        };
+      };
       hackage = {
         rev = "feae602db08e48b1d1a716fc9c4c8348aa5df17a";
         sha256 = "0m7qr2nmfj6lbf9h6wfyapphcqwarqw3319zkdxqwzhbis81hlcb";
