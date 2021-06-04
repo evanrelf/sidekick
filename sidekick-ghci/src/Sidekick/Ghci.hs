6b8ce83c8f2c51b427ab5a6511b8331340bb2562
Evan Relf <evan@evanrelf.com>
2021-06-03 22:13:39 -0700

    Revert from "safe/unsafe" to "high/low level"

--- sidekick-ghci/src/Sidekick/Ghci.hs
+++ sidekick-ghci/src/Sidekick/Ghci.hs
@@ -13,18 +13,17 @@ module Sidekick.Ghci
     -- * Start GHCi session
   , withGhci
 
-    -- * Safe operations
-    -- | Higher-level, safe wrappers around 'send' and 'receive'. Functions take
-    -- a lock on the GHCi session to prevent concurrent access, and calls to
-    -- 'send' are always followed by 'receive' to ensure the GHCi session is in
-    -- a good state for the next command.
+    -- * High-level operations
+    -- | Safe wrappers around 'send' and 'receive'. Functions take a lock on the
+    -- GHCi session to prevent concurrent access, and calls to 'send' are always
+    -- followed by 'receive' to ensure the GHCi session is in a good state for
+    -- the next command.
   , run
   , run_
 
-    -- * Unsafe operations
-    -- | Lower-level primitives for more direct manipulation of the GHCi
-    -- session, providing no checks or guarantees that you maintain a good
-    -- state.
+    -- * Low-level operations
+    -- | Primitives for more direct manipulation of the GHCi session, providing
+    -- no checks or guarantees that you maintain a good state.
   , send
   , receive
   , receiveStreaming
