1aa81aa713e937b59eb297a047939e18424ce30f
Evan Relf <evan@evanrelf.com>
2021-06-23 22:23:07 -0700

    Outline message parsers

--- sidekick/src/Sidekick/Parsers.hs
+++ sidekick/src/Sidekick/Parsers.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 
+-- TODO: Test parsers
+
 module Sidekick.Parsers
   ( Message (..)
   , LoadingMessage (..)
   , DiagnosticMessage (..)
   , Severity (..)
   , LoadConfigMessage (..)
+  , parseMessage
+  , parseLoadingMessage
+  , parseDiagnosticMessage
+  , parseLoadConfigMessage
   , parseCwd
   , parseModules
   , parseModule
@@ -54,6 +60,28 @@ data Severity
 newtype LoadConfigMessage = LoadConfigMessage
   { path :: FilePath
   }
+
+
+parseMessage :: Parser Message
+parseMessage = asum
+  [ Loading <$> parseLoadingMessage
+  , Diagnostic <$> parseDiagnosticMessage
+  , LoadConfig <$> parseLoadConfigMessage
+  ]
+
+
+-- [1 of 2] Compiling GHCi             ( GHCi.hs, interpreted )
+parseLoadingMessage :: Parser LoadingMessage
+parseLoadingMessage = undefined
+
+
+parseDiagnosticMessage :: Parser DiagnosticMessage
+parseDiagnosticMessage = undefined
+
+
+-- Loaded GHCi configuration from C:\Neil\ghcid\.ghci
+parseLoadConfigMessage :: Parser LoadConfigMessage
+parseLoadConfigMessage = undefined
 
 
 parseCwd :: Parser FilePath
