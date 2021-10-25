1503016864deab252fa6261e2ec6e13a2c9b2d64
Evan Relf <evan@evanrelf.com>
2021-10-24 18:08:32 -0700

    Refactor Haskell overlay machinery
    Now supports arbitrary extensions to `haskellPackages`, in a
    customizable order.

--- nix/overlays/haskell-packages.nix
+++ nix/overlays/haskell-packages.nix
@@ -3,8 +3,7 @@
 pkgsFinal: pkgsPrev:
 
 let
-  overrideHaskellPackages = attrs:
-    import ../lib/override-haskell-packages.nix attrs pkgsFinal pkgsPrev;
+  haskellOverlay = import ../lib/haskell-overlay.nix pkgsFinal pkgsPrev;
 
   source = path:
     let
@@ -17,52 +16,56 @@ let
     '';
 
 in
-overrideHaskellPackages {
+haskellOverlay.mkOverlay {
   inherit compiler;
-
-  packages = haskellPackagesFinal: haskellPackagesPrev: {
-    "sidekick" = source ../../sidekick;
-    "sidekick-ghci" = source ../../sidekick-ghci;
-    "sidekick-ghci-json" = source ../../sidekick-ghci-json;
-    "sidekick-ghci-parsers" = source ../../sidekick-ghci-parsers;
-
-    "optics" = "0.4";
-    "optics-core" = "0.4";
-    "optics-extra" = "0.4";
-    "optics-th" = "0.4";
-    "streamly" = "0.8.0";
-  };
-
-  overrideCabal = haskellPackagesFinal: haskellPackagesPrev:
-    let
-      enableFusionPlugin = old: {
-        configureFlags = (old.configureFlags or [ ]) ++ [ "-ffusion-plugin" ];
-        libraryHaskellDepends = (old.libraryHaskellDepends or [ ]) ++ [
-          haskellPackagesFinal.fusion-plugin
-        ];
-      };
-    in
-    {
-      "sidekick" = old: enableFusionPlugin old;
-
-      # TODO: Fix tests failing in Nix
-      "sidekick-ghci" = old: enableFusionPlugin old // {
-        doCheck = false;
-      };
-
-      "streamly" = old: {
-        # `cabal2nix` doesn't add `Cocoa` to `streamly-0.8.0`'s `buildInputs`
-        # automatically.
-        # https://github.com/NixOS/cabal2nix/issues/470
-        libraryHaskellDepends = (old.libraryHaskellDepends or [ ]) ++
-          pkgsPrev.lib.optionals
-            pkgsPrev.stdenv.isDarwin
-            [ pkgsFinal.darwin.apple_sdk.frameworks.Cocoa ];
-      };
-    };
 
   hackage = {
     rev = "74d57b6403ac6b672eacaac9fd68fd7edf54d937";
     sha256 = "18xryn7b6wqlizrfvhnv3jvpgmmlzrx9dkv86r4g8x886ggv23fh";
   };
+
+  extensions = [
+    (haskellOverlay.sources (haskellPackagesFinal: haskellPackagesPrev: {
+      "sidekick" = source ../../sidekick;
+      "sidekick-ghci" = source ../../sidekick-ghci;
+      "sidekick-ghci-json" = source ../../sidekick-ghci-json;
+      "sidekick-ghci-parsers" = source ../../sidekick-ghci-parsers;
+    }))
+
+    (haskellPackagesFinal: haskellPackagesPrev: {
+      "optics" = haskellPackagesFinal."optics_0_4";
+      "optics-core" = haskellPackagesFinal."optics-core_0_4";
+      "optics-extra" = haskellPackagesFinal."optics-extra_0_4";
+      "optics-th" = haskellPackagesFinal."optics-th_0_4";
+      "streamly" = haskellPackagesFinal."streamly_0_8_0";
+    })
+
+    (haskellOverlay.overrideCabal (haskellPackagesFinal: haskellPackagesPrev:
+      let
+        enableFusionPlugin = prev: {
+          configureFlags = (prev.configureFlags or [ ]) ++ [ "-ffusion-plugin" ];
+          libraryHaskellDepends = (prev.libraryHaskellDepends or [ ]) ++ [
+            haskellPackagesFinal.fusion-plugin
+          ];
+        };
+      in
+      {
+        "sidekick" = prev: enableFusionPlugin prev;
+
+        # TODO: Fix tests failing in Nix
+        "sidekick-ghci" = prev: enableFusionPlugin prev // {
+          doCheck = false;
+        };
+
+        "streamly" = prev: {
+          # `cabal2nix` doesn't add `Cocoa` to `streamly-0.8.0`'s `buildInputs`
+          # automatically.
+          # https://github.com/NixOS/cabal2nix/issues/470
+          libraryHaskellDepends = (prev.libraryHaskellDepends or [ ]) ++
+            pkgsPrev.lib.optionals
+              pkgsPrev.stdenv.isDarwin
+              [ pkgsFinal.darwin.apple_sdk.frameworks.Cocoa ];
+        };
+      }))
+  ];
 }
