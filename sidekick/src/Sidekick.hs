55bd954331f18bcdc5c9d847d611ee82775210d0
Evan Relf <evan@evanrelf.com>
2023-03-17 20:19:09 -0700

    Use new record syntax

--- sidekick/src/Sidekick.hs
+++ sidekick/src/Sidekick.hs
@@ -1,12 +1,10 @@
 module Sidekick (main) where
 
-import Sidekick.Options (Options (..))
-
 import qualified Sidekick.Lsp as Lsp
 import qualified Sidekick.Options as Options
 
 
 main :: IO ()
 main = do
-  Options{command, directory} <- Options.getOptions
+  options <- Options.getOptions
   print =<< Lsp.start
